9cf25b454a9bdd765aa9ec43af7e574a85edfe9f
beruberu1995 <buakong05171329@gmail.com>
2025-12-14 00:42:06 +0700

    Update start_page.dart

--- smart_cat_water_bowl/lib/pages/start_page.dart
+++ smart_cat_water_bowl/lib/pages/start_page.dart
@@ -1,1 +1,29 @@
-hello
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Meow Flow',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/');
+              },
+              child: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
