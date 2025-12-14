21d23deac9106ba025fcfb728b10747c824ccd59
NINO <phakin8473@gmail.com>
2025-12-14 10:09:30 +0700

    เชื่อมCloudfirestore

--- smart_cat_water_bowl/android/app/build.gradle.kts
+++ smart_cat_water_bowl/android/app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +43,14 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// android/app/build.gradle.kts
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
--- smart_cat_water_bowl/lib/main.dart
+++ smart_cat_water_bowl/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_cat_water_bowl/firebase_options.dart';
 import 'pages/start_page.dart';
 import 'pages/welcome_page.dart';
 import 'pages/login_page.dart';
@@ -6,7 +8,9 @@ import 'pages/register_page.dart';
 import 'pages/home_page.dart';
 import 'pages/information_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
